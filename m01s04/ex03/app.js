3c666168820d8acfb7bc8081e54a0f9d0a8940ab
Ciurea Cristian <123569444+Ciureacristian@users.noreply.github.com>
2023-09-14 06:47:02 -0900

    m01-01-04

---
+++ m01s04/ex03/app.js
@@ -0,0 +1,44 @@
+var routes = [
+  {
+    name: 'Home',
+    route: '/',
+    text: 'Te afli pe homepage',
+  },
+  {
+    name: 'Contact',
+    route: '/contact',
+    text: 'Te afli pe pagina de contact',
+  },
+  {
+    name: 'About',
+    route: '/about',
+    text: 'Te afli pe pagina de about',
+  },
+];
+var selectedRoute = '/about-us';
+
+switch (selectedRoute) {
+  case '/':
+    var route = routes.find(function (route) {
+      return route.route === selectedRoute;
+    });
+    console.log(route.text);
+    break;
+
+  case '/about':
+    var route = routes.find(function (route) {
+      return route.route === selectedRoute;
+    });
+    console.log(route.text);
+    break;
+
+  case '/contact':
+    var route = routes.find(function (route) {
+      return route.route === selectedRoute;
+    });
+    console.log(route.text);
+    break;
+
+  default:
+    console.log('404');
+}
