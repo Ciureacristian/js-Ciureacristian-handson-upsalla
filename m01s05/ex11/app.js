3d130fac93b91fed76f0457474649d6a85347c90
Ciurea Cristian <123569444+Ciureacristian@users.noreply.github.com>
2023-10-02 06:02:40 -0900

    Curs 06 ex 03-06 si 05-11

--- m01s05/ex11/app.js
+++ m01s05/ex11/app.js
@@ -1,77 +1,52 @@
-function calculateSurface(...arguments) {
-  switch (arguments.length) {
+function calculateSurface(...args) {
+  switch (args.length) {
     case 1:
       const side = args[0];
-      if (arguments.length === 1) {
-        return side * side;
-      }
-      console.log(
-        `Suprafața unui pătrat cu latura ${side} este ${side * side}`,
-      );
-      break;
+      const squareArea = side * side;
+      return squareArea;
     case 2:
       const length = args[0];
       const width = args[1];
-      if (arguments.length === 2) {
-        return width * length;
-      }
-      break;
+      const rectangleArea = length * width;
+      return rectangleArea;
     case 3:
       const lengthP = args[0];
       const widthP = args[1];
       const heightP = args[2];
-      if (arguments.length === 3) {
-        return 1 * (lengthP * widthP + lengthP * heightP + widthP * heightP);
-      }
-      break;
+      const parallelepipedArea =
+        2 * (lengthP * widthP + lengthP * heightP + widthP * heightP);
+      return parallelepipedArea;
+    default:
+      return null;
   }
 }
 
 function calculateCircleArea(radius) {
   const area = Math.PI * radius * radius;
-  console.log(`Suprafața unui cerc cu rază ${radius} este ${area.toFixed(2)}`);
+  return area;
 }
 
-// Funcția supraincarcată pentru calcularea suprafețelor
-function calculateSurface(...args) {
-  switch (args.length) {
-    case 1:
-      // Dacă primim un singur argument, calculăm suprafața unui pătrat
-      const side = args[0];
-      console.log(
-        `Suprafața unui pătrat cu latura ${side} este ${side * side}`,
-      );
-      break;
-    case 2:
-      // Dacă primim doi argumente, calculăm suprafața unui dreptunghi
-      const length = args[0];
-      const width = args[1];
-      console.log(
-        `Suprafața unui dreptunghi cu lungime ${length} și lățime ${width} este ${
-          length * width
-        }`,
-      );
-      break;
-    case 3:
-      // Dacă primim trei argumente, calculăm suprafața totală a unui paralelipiped
-      const lengthP = args[0];
-      const widthP = args[1];
-      const heightP = args[2];
-      console.log(
-        `Suprafața totală a unui paralelipiped cu lungime ${lengthP}, lățime ${widthP} și înălțime ${heightP} este ${
-          2 * (lengthP * widthP + lengthP * heightP + widthP * heightP)
-        }`,
-      );
-      break;
-    default:
-      console.log('Numărul de argumente este invalid.');
-  }
-}
+const squareResult = calculateSurface(2);
+const rectangleResult = calculateSurface(6, 10);
+const parallelepipedResult = calculateSurface(3, 4, 7);
+const circleResult1 = calculateCircleArea(2);
+const circleResult2 = calculateCircleArea(20);
+const circleResult3 = calculateCircleArea(16);
 
-// Obiectul "pet" cu metodele getName, getSpecies și getAge
+console.log(`Suprafata unui patrat cu latura 2 este ${squareResult}`);
+console.log(
+  `Suprafata unui dreptunghi cu lungime 6 și latime 10 este ${rectangleResult}`,
+);
+console.log(
+  `Suprafata totala a unui paralelipiped cu lungime 3, latime 4 și inaltime 7 este ${parallelepipedResult}`,
+);
+console.log(`${circleResult1}.`);
+console.log(`${circleResult2}.`);
+console.log(`${circleResult3}.`);
+
 const pet = {
   name: 'Rex',
-  species: 'câine',
+  species: 'caine',
   age: 5,
   getName: function () {
     return this.name;
@@ -84,17 +59,14 @@ const pet = {
   },
 };
 
-// Afișăm propoziția folosind metodele obiectului "pet"
 const petName = pet.getName();
 const petSpecies = pet.getSpecies();
 const petAge = pet.getAge();
-console.log(`${petName} este ${petSpecies} și are ${petAge} ani.`);
+console.log(`${petName} este ${petSpecies} si are ${petAge} ani.`);
 
-// Calculăm și afișăm vârsta animalului folosind metoda getAge
 const animalAge = pet.getAge();
 console.log(`Animalul meu are ${animalAge} ani.`);
 
-// Apelăm funcția calculateCircleArea pentru câteva valori de rază
 calculateCircleArea(2);
 calculateCircleArea(20);
 calculateCircleArea(16);
--- m01s06/ex04/app.js
+++ m01s06/ex04/app.js
@@ -1,5 +1,25 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-      
+(function () {
+  const paragrapheElement = document.createElement('p');
+  paragrapheElement.classList.add('message');
+  document.body.append(paragrapheElement);
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const stage = document.querySelector('.stage');
+
+    stage.addEventListener('mouseover', function () {
+      const message = 'Mouseul este pe scena';
+      console.log(message);
+      showMessage(message);
+
+      stage.addEventListener('mouseout', function () {
+        const message = 'Mouseul nu este pe scena';
+        showMessage(message);
+        console.log(message);
+      });
+    });
   });
+
+  function showMessage(message) {
+    paragrapheElement.innerText = message;
+  }
 })();
